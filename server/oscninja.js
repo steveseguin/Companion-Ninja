053f4d47844fc16b4f5a2e45744cada6667d6dca
Steve Seguin <steve@seguin.email>
2022-09-06 05:56:34 -0400

    Update oscninja.js
    support for callbacks

--- server/oscninja.js
+++ server/oscninja.js
@@ -1,5 +1,6 @@
-//sudo add-apt-repository ppa:certbot/certbot # Add the certbot repository, if desired
-//sudo apt-get install certbot -y # Install certbot required for the HTTPS certificate, if desired
+// If needing certs, you can try certbot; it's free, but requires updates every 3 months.
+//sudo add-apt-repository ppa:certbot/certbot # Add the certbot repository
+//sudo apt-get install certbot -y # Install certbot required for the HTTPS certificate
 
 "use strict";
 var fs = require("fs");
@@ -10,46 +11,85 @@ var app = express();
 var WebSocket = require("ws");
 var cors = require('cors');
 
-//const key = fs.readFileSync("/etc/letsencrypt/live/api.vdo.ninja/privkey.pem");
-//const cert = fs.readFileSync("/etc/letsencrypt/live/api.vdo.ninja/fullchain.pem");
-
+// make sure you have SSL certs installed; if using Certbot, they should be like this:
+//const key = fs.readFileSync("/etc/letsencrypt/live/wss.contribute.cam/privkey.pem");
+//const cert = fs.readFileSync("/etc/letsencrypt/live/wss.contribute.cam/fullchain.pem");
 //var server = https.createServer({key,cert}, app);
-var server = http.createServer(app); // in this app, I'm going to rely on Cloudflare for SSL. keep life easy
+
+var server = http.createServer(app); // in this app, I'm going to rely on Cloudflare for SSL. keeps life easy
 var websocketServer = new WebSocket.Server({ server });
+
+var callback = {};
 
 app.use(express.json());
 
 app.use(cors({
     origin: '*'
 }));
-          
+
 app.get('/', (req, res) => {
   res.send("0")
-})       
-        
-app.get('/:room', (req, res) => {
-  res.send("0");
-});   
+})
 
-app.post('/:room', (req, res) => { 
+app.get('/:room', async (req, res) => {
   var room = req.params.room + "";
   var counter = 0;
-  var msg = JSON.stringify(req.body);
+  var pid = Math.random().toString(36).substr(2, 9);
+  var promise = new Promise((resolve, reject) => {
+        callback[pid] = {};
+        callback[pid].resolve = resolve;
+        callback[pid].reject = reject;
+        setTimeout((pid) => {
+            if (callback[pid]){
+                callback[pid].resolve('timeout');
+                delete callback[pid];
+            }
+        }, 5000, pid);
+  });
+
+  var msg = {};
+  msg.action = "getDetails";
+  msg.value = "value";
+  msg.get = pid;
+  msg = JSON.stringify(msg);
+
   websocketServer.clients.forEach( client => {
         if (client.room === room){
-                try {
+                try{
                         client.send(msg);
                         counter+=1;
                 } catch(e){}
-                  }
+        }
   });
-  res.send(counter+"");
+
+  if (counter==0){
+        res.send("failed");
+        callback[pid].resolve('failed');
+  } else {
+        var cb = await promise.then(function(x){return x;}).catch(function(x){return x;});
+        res.send(""+cb);
+  }
 });
 
-app.put('/:room', (req, res) => {
+app.post('/:room', async (req, res) => {
   var room = req.params.room + "";
   var counter = 0;
-  var msg = JSON.stringify(req.body);
+  var pid = Math.random().toString(36).substr(2, 9);
+  var promise = new Promise((resolve, reject) => {
+        callback[pid] = {};
+        callback[pid].resolve = resolve;
+        callback[pid].reject = reject;
+        setTimeout((pid) => {
+            if (callback[pid]){
+                callback[pid].resolve('timeout');
+                delete callback[pid];
+            }
+        }, 5000, pid);
+  });
+  var msg = req.body;
+  msg.get = pid;
+  msg = JSON.stringify(msg);
+
   websocketServer.clients.forEach( client => {
         if (client.room === room){
                 try {
@@ -58,30 +98,106 @@ app.put('/:room', (req, res) => {
                 } catch(e){}
         }
   });
-  res.end();
+  if (counter==0){
+        res.send("failed");
+        callback[pid].resolve('failed');
+  } else {
+        var cb = await promise.then(function(x){return x;}).catch(function(x){return x;});
+        res.send(""+cb);
+  }
 });
 
-app.get('/:room/:action/:value', (req, res) => {
+app.put('/:room', async (req, res) => {
+  var room = req.params.room + "";
+  var counter = 0;
+  var pid = Math.random().toString(36).substr(2, 9);
+  var promise = new Promise((resolve, reject) => {
+        callback[pid] = {};
+        callback[pid].resolve = resolve;
+        callback[pid].reject = reject;
+        setTimeout((pid) => {
+            if (callback[pid]){
+                callback[pid].resolve('timeout');
+                delete callback[pid];
+            }
+        }, 5000, pid);
+  });
+  var msg = req.body;
+  msg.get = pid;
+  msg = JSON.stringify(msg);
+
+  websocketServer.clients.forEach( client => {
+        if (client.room === room){
+                try {
+                        client.send(msg);
+                        counter+=1;
+                } catch(e){}
+        }
+  });
+  if (counter==0){
+        res.send("failed");
+        callback[pid].resolve('failed');
+  } else {
+        var cb = await promise.then(function(x){return x;}).catch(function(x){return x;});
+        res.send(""+cb);
+  }
+});
+
+app.get('/:room/:action/:value', async (req, res) => {
+
+  var pid = Math.random().toString(36).substr(2, 9);
+  var promise = new Promise((resolve, reject) => {
+        callback[pid] = {};
+        callback[pid].resolve = resolve;
+        callback[pid].reject = reject;
+        setTimeout((pid) => {
+            if (callback[pid]){
+                callback[pid].resolve('timeout');
+                delete callback[pid];
+            }
+        }, 5000, pid);
+  });
   var msg = {};
   msg.action = req.params.action;
   msg.value = req.params.value;
+  msg.get = pid;
   msg = JSON.stringify(msg);
 
   var room = req.params.room + "";
   var counter = 0;
   websocketServer.clients.forEach( client => {
-          if (client.room === room){
+	  if (client.room === room){
                 try{
                         client.send(msg);
                         counter+=1;
                 } catch(e){}
         }
   });
-  res.send(counter+"");
+  if (counter==0){
+        res.send("failed");
+        callback[pid].resolve('failed');
+  } else {
+        var cb = await promise.then(function(x){return x;}).catch(function(x){return x;});
+        res.send(""+cb);
+  }
 });
 
-app.get('/:room/:action', (req, res) => {
+app.get('/:room/:action', async (req, res) => {
+
+  var pid = Math.random().toString(36).substr(2, 9);
+  var promise = new Promise((resolve, reject) => {
+        callback[pid] = {};
+        callback[pid].resolve = resolve;
+        callback[pid].reject = reject;
+        setTimeout((pid) => {
+            if (callback[pid]){
+                callback[pid].resolve('timeout');
+                delete callback[pid];
+            }
+        }, 5000, pid);
+  });
   var msg = {};
+  msg.get = pid;
   msg.action = req.params.action;
   msg = JSON.stringify(msg);
 
@@ -95,54 +211,124 @@ app.get('/:room/:action', (req, res) => {
                 } catch(e){}
         }
   });
-  res.send(counter+"");
+  if (counter==0){
+        res.send("failed");
+        callback[pid].resolve('failed');
+  } else {
+        var cb = await promise.then(function(x){return x;}).catch(function(x){return x;});
+        res.send(""+cb);
+  }
 });
 
-app.get('/:room/:action/:target/:value', (req, res) => {
+app.get('/:room/:action/:target/:value', async (req, res) => {
+
+  var pid = Math.random().toString(36).substr(2, 9);
+  var promise = new Promise((resolve, reject) => {
+        callback[pid] = {};
+        callback[pid].resolve = resolve;
+        callback[pid].reject = reject;
+        setTimeout((pid) => {
+            if (callback[pid]){
+                callback[pid].resolve('timeout');
+                delete callback[pid];
+            }
+        }, 5000, pid);
+  });
   var msg = {};
   msg.action = req.params.action;
   msg.value = req.params.value;
   msg.target = req.params.target;
+  msg.get = pid;
   msg = JSON.stringify(msg);
   var room = req.params.room +"";
   var counter = 0;
+
   websocketServer.clients.forEach( client => {
-        if (client.room === room){
-                try{
-                        client.send(msg);
-                        counter+=1;
-                } catch(e){}
+  	if (client.room === room){
+		try{
+	              	client.send(msg);
+			counter+=1;
+		} catch(e){}
         }
   });
-  res.send(counter+"");
+  if (counter==0){
+        res.send("failed");
+        callback[pid].resolve('failed');
+  } else {
+        var cb = await promise.then(function(x){return x;}).catch(function(x){return x;});
+        res.send(""+cb);
+  }
 });
 
 websocketServer.on('connection', (webSocketClient) => {
     var room = false;
+    var out = false;
     webSocketClient.on('message', (message) => {
-            if (!webSocketClient.room){
-                try{
+	    if (!webSocketClient.room){
+		try{
                     var msg = JSON.parse(message);
-                    if ("join" in msg){
-                        room = msg.join+"";
-                        webSocketClient.room = room;
-                    }
-                    return;
-                } catch(e){return;}
-            }
-            websocketServer.clients.forEach( client => {
-                    if (client.room === room){
-                        if (webSocketClient!=client){
-                                try{
-                                        client.send(message.toString());
-                                } catch(e){}
+		    if ("join" in msg){
+			room = msg.join+"";
+			webSocketClient.room = room;
+
+			if ("out" in msg){
+                                webSocketClient.out = msg.out;
+                                out = msg.out;
+                        } else {
+                                webSocketClient.out = false;
                         }
-                    }
-            });
+                        if ("in" in msg){
+                                webSocketClient.inn = msg.in;
+                        } else {
+                                webSocketClient.inn = false;
+                        }
+		    }
+		    return;
+	        } catch(e){return;}
+	    }
+	    var msg = JSON.parse(message);
+            if (msg.callback && ("get" in msg.callback)){
+		if (callback[msg.callback.get]){
+			if ("result" in msg.callback){
+				if (typeof msg.callback.result=='object'){
+					callback[msg.callback.get].resolve(JSON.stringify(msg.callback.result));
+				} else {
+					callback[msg.callback.get].resolve(msg.callback.result);
+				}
+			} else {
+				try {
+					var msg = message.callback;
+					delete msg.get;
+					callback[msg.callback.get].resolve(JSON.stringify(msg));
+				}catch(e){}
+			}
+		}
+		return;
+	    }
+
+	    websocketServer.clients.forEach( client => {
+		    if (client.room === room){
+		    	if (webSocketClient!=client){
+				if (client.inn && out){
+                                        if (client.inn == out){
+                                                try{
+                                                        client.send(message.toString());
+                                                } catch(e){}
+                                        }
+                                } else if (client.inn || out){
+                                        // skip
+                                } else {
+                                        try{
+                                                client.send(message.toString());
+                                        } catch(e){}
+                                }
+		    	}
+		    }
+	    });
     });
     webSocketClient.on('close', function(reasonCode, description) {
     });
 
 });
-//server.listen(443, () => {console.log(`Server started on port 443`) }); // if using a local cert
-server.listen(80, () => {console.log(`Server started on port 80`) }); // if using cloudflare ssl
+//server.listen(443, () => {console.log(`Server started on port 443`) });
+server.listen(80, () => {console.log(`Server started on port 80`) });
